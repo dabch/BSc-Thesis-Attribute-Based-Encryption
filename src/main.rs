da46314643362959b0b624c0b85dea6074785d39
Daniel Bücheler <dabch@posteo.de>
2020-12-10 18:35:07 +0100

    implement setup() on basic YaoABE struct

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,44 @@
 //use elliptic_curve;
-use p256;
-//use rand::OsRng;
+use p256::elliptic_curve::{Group, Field};
+use p256::{Scalar, ProjectivePoint, AffinePoint};
+use rand_core::OsRng;
+use std::collections::HashMap;
 
 fn main() {
   
-  let g = p256::ProjectivePoint::generator();
   
-  let s1 = p256::Scalar::one() + p256::Scalar::one();
-  let s2 = p256::Scalar::one() + p256::Scalar::one() + p256::Scalar::one() + p256::Scalar::one() +
-  p256::Scalar::one() + p256::Scalar::one() + p256::Scalar::one() + p256::Scalar::one() +
-  p256::Scalar::one() + p256::Scalar::one() + p256::Scalar::one() + p256::Scalar::one() +
-  p256::Scalar::one() + p256::Scalar::one() + p256::Scalar::one() + p256::Scalar::one() +
-  p256::Scalar::one() + p256::Scalar::one();
+  let atts = vec!["student", "tum", "over21"];
+  let es = YaoABE::setup(&atts);
+  println!("{:#?}", es);
+}
 
-  let p1 = g * s1;
-  let p2 = g * s2;
+#[derive(Debug)]
+struct YaoABE<'a> {
+  atts: HashMap<&'a str, (Scalar, ProjectivePoint)>,
+  pk: ProjectivePoint,
+  master_secret: Scalar,
+}
 
-  assert_eq!(p2 * s1, p1 * s2);
-  assert_ne!(p2 * s2, p1 * s1);
-  println!("Alice got: {:?}", p2 * s1);
-  println!("Bob got:   {:?}", p1 * s2);
+impl YaoABE<'_> {
+  fn setup(att_names: &Vec<&'static str>) -> Self {
+    let s = Scalar::random(&mut OsRng);
+    let g = ProjectivePoint::random(&mut OsRng);
+    
+    let mut att_map: HashMap<&str, (Scalar, ProjectivePoint)> = HashMap::new();
+    for attr in att_names {
+      let si = Scalar::random(&mut OsRng);
+      let gi = g * si;
+      att_map.insert(attr, (si, gi));
+    }
 
-  println!();
-  println!("DH secret: {}", (p2 * s1).to_affine().x);
+    YaoABE {
+      atts: att_map,
+      pk: g,
+      master_secret: s,
+    }
+  }
+
+  //fn encrypt(pk: ProjectivePoint, atts: &Vec<&str>, plaintext: Vec<u8>) -> {
+//
+  //}
 }
