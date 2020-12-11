01256fa4b3b5385614a8458cad4c895af61a86eb
Daniel Bücheler <dabch@posteo.de>
2020-12-11 19:17:55 +0100

    add untested keygen implementation

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,30 @@ fn main() {
   println!("{:#?}", es);
   let public = es.to_public();
   println!("\n public params:\n{:#?}", public);
+
+  let access_structure = AccessStructure::Node(
+    2,
+    vec![AccessStructure::Leaf("student"), AccessStructure::Leaf("tum")]
+  );
+
+  println!("{:#?}", access_structure);
+
+  // let poly = Polynomial(vec![1, 2, 3, 4].iter().map(|x| Scalar::from(x.clone())).collect());
+  
+  // println!("f(1) = {:?}", poly.eval(Scalar::from(1)));
+
+  // println!("f(4) = {:?}", poly.eval(Scalar::from(4)));
+  // println!("f(8100) = {:?}", poly.eval(Scalar::from(8100)));
+  // println!("{:?}", Scalar::from(1225));
+
+  // println!("randgen(125, 5) = {:#?}", Polynomial::randgen(Scalar::from(125), 5));
+
+  // let vecvec = vec![vec![1, 2, 3, 4], vec![5, 6, 7], vec![8, 9, 10, 11]];
+
+  // println!("{:?}", vecvec.into_iter().flatten().collect::<Vec<i64>>());
+
+  let priv_key = es.keygen(access_structure);
+  println!("{:?}", priv_key);
 }
 
 #[derive(Debug)]
@@ -28,11 +52,38 @@ struct YaoABEPublic<'a> {
   pk: ProjectivePoint,
 }
 
+#[derive(Debug)]
+enum AccessStructure<'a> {
+  Node(u64, Vec<AccessStructure<'a>>), // threshold, children
+  Leaf(&'a str),
+}
+
+#[derive(Debug)]
 struct YaoABECiphertext<'a> {
   //omega: &Vec<&str>, // set of attributes (implied by c_i)
   c: Vec<u8>, // actual ciphertext (output of AES)
   mac: Vec<u8>, // mac over the cleartext (TODO better encrypt-then-mac?)
   c_i: HashMap<&'a str, ProjectivePoint>, // attributes and their respective curve elements
+}
+
+#[derive(Debug)]
+struct YaoABEKey(Vec<Scalar>);
+
+#[derive(Debug)]
+struct Polynomial(Vec<Scalar>);
+
+impl Polynomial {
+  fn eval(&self, x: Scalar) -> Scalar {
+    self.0.iter().rev().fold(Scalar::zero(), |acc, c| c + &(x * acc))
+  }
+
+  fn randgen(a0: Scalar, coeffs: u64) -> Polynomial {
+    let mut coefficients = vec![a0];
+
+    coefficients.extend((1..coeffs).map(|x| Scalar::random(&mut OsRng)));
+    assert_eq!(coefficients.len() as u64, coeffs);
+    Polynomial(coefficients)
+  }
 }
 
 impl<'a> YaoABEPrivate<'a> {
@@ -59,6 +110,49 @@ impl<'a> YaoABEPrivate<'a> {
       atts: self.atts.iter().map(|(k, (s, p))| (k.clone(), p)).collect(),
       pk: self.pk,
     }
+  }
+
+  fn keygen(
+    &self,
+    access_structure: AccessStructure
+  ) ->
+    YaoABEKey
+  { 
+    // if let AccessStructure::Node(thresh, children) = access_structure {
+    //   let final_ds = self.keygen_internal(access_structure, parent_poly: &Polynomial, index: Scalar)
+    // }
+    let final_ds = self.keygen_internal(
+      &access_structure,
+      &Polynomial::randgen(self.master_secret, 1),
+      Scalar::from(1)
+    );
+    YaoABEKey(final_ds)
+  }
+
+  fn keygen_internal(&self,
+    tree: &AccessStructure,
+    parent_poly: &Polynomial,
+    index: Scalar
+  ) ->
+    Vec<Scalar>
+  {
+    match tree {
+      AccessStructure::Leaf(attr_name) => {
+        let q_of_zero = parent_poly.eval(Scalar::zero());
+        let (s, _) = self.atts.get(attr_name).unwrap();
+        let s_inverse = s.invert().unwrap();
+        return vec![q_of_zero * s_inverse];
+      },
+      AccessStructure::Node(thresh, children) => {
+        let own_poly = Polynomial::randgen(parent_poly.eval(index), thresh.clone());
+        let children_res: Vec<Scalar> = children.iter().enumerate().
+          map(|(i, child)| self.keygen_internal(child, &own_poly, Scalar::from(i as u64))).
+          flatten().
+          collect();
+        return children_res;
+      }
+    }
+    Vec::new()
   }
 
   fn encrypt(
