8b8aa9ba91b4a3b021dad318dfd4a49da9a2feb4
Daniel Bücheler <dabch@posteo.de>
2020-12-11 15:24:45 +0100

    add basic non-functional encrypt (without touching the plain text at all)

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,40 @@
 use p256::elliptic_curve::{Group, Field};
 use p256::{Scalar, ProjectivePoint, AffinePoint};
 use rand_core::OsRng;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+
 
 fn main() {
   
   
   let atts = vec!["student", "tum", "over21"];
-  let es = YaoABE::setup(&atts);
+  let es = YaoABEPrivate::setup(&atts);
   println!("{:#?}", es);
-  let atts_pub: HashMap<_, _> = es.atts.iter().map(| (k, (s, p)) | (k, p)).collect();
-  println!("\n atts_pub:\n{:#?}", atts_pub);
+  let public = es.to_public();
+  println!("\n public params:\n{:#?}", public);
 }
 
 #[derive(Debug)]
-struct YaoABE<'a> {
+struct YaoABEPrivate<'a> {
   atts: HashMap<&'a str, (Scalar, ProjectivePoint)>,
   pk: ProjectivePoint,
   master_secret: Scalar,
 }
 
-impl YaoABE<'_> {
+#[derive(Debug)]
+struct YaoABEPublic<'a> {
+  atts: HashMap<&'a str, &'a ProjectivePoint>,
+  pk: ProjectivePoint,
+}
+
+struct YaoABECiphertext<'a> {
+  //omega: &Vec<&str>, // set of attributes (implied by c_i)
+  c: Vec<u8>, // actual ciphertext (output of AES)
+  mac: Vec<u8>, // mac over the cleartext (TODO better encrypt-then-mac?)
+  c_i: HashMap<&'a str, ProjectivePoint>, // attributes and their respective curve elements
+}
+
+impl<'a> YaoABEPrivate<'a> {
   fn setup(att_names: &Vec<&'static str>) -> Self {
     let s = Scalar::random(&mut OsRng);
     let g = ProjectivePoint::random(&mut OsRng);
@@ -32,15 +46,44 @@ impl YaoABE<'_> {
       let gi = g * si;
       att_map.insert(attr, (si, gi));
     }
-
-    YaoABE {
+    
+    YaoABEPrivate {
       atts: att_map,
       pk: g,
       master_secret: s,
     }
   }
 
-  //fn encrypt(pk: ProjectivePoint, atts: &Vec<&str>, plaintext: Vec<u8>) -> {
-    
-  //}
+  fn to_public(&self) -> YaoABEPublic {
+    YaoABEPublic {
+      atts: self.atts.iter().map(|(k, (s, p))| (k.clone(), p)).collect(),
+      pk: self.pk,
+    }
+  }
+
+  fn encrypt(
+    params: YaoABEPublic,
+    atts: &'a Vec<&'a str>,
+    plaintext: &'a Vec<u8>,
+    ) -> YaoABECiphertext<'a>
+  {
+    let (k, Cprime) = loop {
+      let k = Scalar::random(&mut OsRng);
+      let Cprime = params.pk * k;
+      if Cprime.is_identity().unwrap_u8() == 0 { break (k, Cprime) };
+    };
+
+    let mut att_cs: HashMap<&str, ProjectivePoint> = HashMap::new();
+    for att in atts {
+      let att_pubkey: &ProjectivePoint = params.atts.get(att).unwrap();
+      let c_i = att_pubkey * &k;
+      att_cs.insert(att, c_i);
+    }
+
+    YaoABECiphertext {
+      c: Vec::new(),
+      mac: Vec::new(),
+      c_i: att_cs,
+    }
+  }
 }
