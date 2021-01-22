64a21cce50f33beeb6c51fc5f2ad722244989d8c
Daniel Bücheler <daniel.buecheler@tum.de>
2021-01-22 18:40:56 +0100

    further tests with rabe-bn on nrf52832

--- pairing-nrf-test/.gdbinit
+++ pairing-nrf-test/.gdbinit
@@ -7,6 +7,7 @@ define nia
 ni
 x/3i $pc
 end
-file target/thumbv7em-none-eabihf/debug/nrf52832-test
+#file target/thumbv7em-none-eabihf/release/nrf52832-test
 target remote localhost:3333
-load target/thumbv7em-none-eabihf/debug/nrf52832-test
+#load target/thumbv7em-none-eabihf/release/nrf52832-test
+load
--- pairing-nrf-test/src/main.rs
+++ pairing-nrf-test/src/main.rs
@@ -1,7 +1,7 @@
 #![no_main]
 #![no_std]
 
-use cortex_m::asm;
+use cortex_m::{asm, iprintln, Peripherals as CortexPeripherals};
 use cortex_m_rt::entry;
 use rtt_target::{rtt_init_print, rprintln};
 use core::panic::PanicInfo;
@@ -16,24 +16,30 @@ use rabe_bn::{G1, G2, Gt, pairing, Group};
 
 #[entry]
 fn main() -> ! {
-    rtt_init_print!();
-    let p = hal::pac::Peripherals::take().unwrap();
-    let mut rng = hal::Rng::new(p.RNG);
+  rtt_init_print!();
+  // let mut c = CortexPeripherals::take().unwrap();
+  let p = hal::pac::Peripherals::take().unwrap();
+  let mut rng = hal::Rng::new(p.RNG);
 
-    p.TIMER0.timer_start(0 as u32);
-    let mut _timer = hal::Timer::new(p.TIMER0);
 
-    rprintln!("getting random elements");
-    let g1 = G1::random(&mut rng);
-    let g2 = G2::random(&mut rng);
-    
-    rprintln!("computing pairing...");
-    let gt = rabe_bn::pairing(g1, g2);
-    rprintln!("done pairing");
-    rprintln!("{:?}", gt);
-    loop {
-      asm::bkpt();  
-    }
+  // let stim = &mut c.ITM.stim[0];
+  // iprintln!(stim, "Hello World!");
+
+  p.TIMER0.timer_start(0 as u32);
+  let mut _timer = hal::Timer::new(p.TIMER0);
+
+  rprintln!("getting random elements");
+  let g1 = G1::random(&mut rng);
+  let g2 = G2::random(&mut rng);
+  
+  // rprintln!("computing pairing...");
+  // let gt = rabe_bn::pairing(g1, g2);
+  // rprintln!("done pairing");
+  // rprintln!("{:?}", gt);
+  // rprintln!("done printing gt");
+  loop {
+    asm::bkpt();  
+  }
 }
 
 #[panic_handler]
