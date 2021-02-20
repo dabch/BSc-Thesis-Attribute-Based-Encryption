8a523f274afd8bf7fac62176f465effe31309b8b
Daniel Buecheler <daniel.buecheler@tum.de>
2021-02-20 19:36:41 +0100

    add pruning and reconstruction test for MSP

--- abe-utils/src/msp.rs
+++ abe-utils/src/msp.rs
@@ -22,10 +22,20 @@ pub enum Label<'a> {
     NotAttribute(&'a str),
 }
 
+impl<'a> Label<'a> {
+    #[allow(dead_code)]
+    fn get_str(&self) -> &'a str {
+        match self {
+            Self::Attribute(s) | Self::NotAttribute(s) => s,
+        }
+    }
+
+}
+
 #[derive(Debug, PartialEq, Eq)]
 pub struct MSP<'a>(Vec<(Label<'a>, Vec<i8, S>), S>);
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct Error;
 
 impl<'a> MSP<'a> {
@@ -33,19 +43,23 @@ impl<'a> MSP<'a> {
     pub fn from_access_tree<'b>(t: AccessTree<'a, 'b>) -> Result<Self, Error> {
         let mut c = 1;
         let mut msp_inner = Self::lw_construction(t, &t[0], &mut c, Vec::from_slice(&[1]).unwrap())?;
+
+        // still need to make sure all vector have the correct length (pad w/ zeroes)
         for (_, row) in msp_inner.iter_mut() {
             row.resize(c as usize, 0)?;
         }
         Ok(MSP(msp_inner))
     }
 
-    fn lw_construction<'b>(tree: AccessTree<'a, 'b>, current_node: &AccessNode<'a>, c: &mut u8, node_label: Vec<i8, S>) -> Result<Vec<(Label<'a>, Vec<i8, S>), S>, Error> {
+    /// recursively applies the construction from Appendix G, "Decentralizing Attribute-Based Encryption", Lewko and Waters, 2011
+    /// 
+    /// Note that the returned matrix's rows might not all have the same length.
+    fn lw_construction<'b>(tree: AccessTree<'a, 'b>, current_node: &AccessNode<'a>, c: &mut u8, v: Vec<i8, S>) -> Result<Vec<(Label<'a>, Vec<i8, S>), S>, Error> {
 
-        // std::println!("{}", *c);
         let (mut res_l, res_r) = match current_node {
-            AccessNode::Or(l, r) => (Self::lw_construction(tree, &tree[*l as usize], c, node_label.clone())?, Self::lw_construction(tree, &tree[*r as usize], c, node_label.clone())?),
+            AccessNode::Or(l, r) => (Self::lw_construction(tree, &tree[*l as usize], c, v.clone())?, Self::lw_construction(tree, &tree[*r as usize], c, v.clone())?),
             AccessNode::And(l, r) => {
-                let mut v_l = node_label.clone();
+                let mut v_l = v;
                 v_l.resize(*c as usize, 0)?;
                 v_l.push(1)?;
 
@@ -53,15 +67,13 @@ impl<'a> MSP<'a> {
                 v_r.resize(*c as usize, 0)?;
                 v_r.push(-1i8)?;
 
-                std::println!("increment c = {} + 1", *c);
                 *c += 1;
 
                 (Self::lw_construction(tree, &tree[*l as usize], c, v_l)?, Self::lw_construction(tree, &tree[*r as usize], c, v_r)?)
             },
             AccessNode::Leaf(s) => {
-                std::println!("labelling {} with c={}", s, *c);
                 let mut row = Vec::new();
-                match row.push((Label::Attribute(s), node_label)) {
+                match row.push((Label::Attribute(s), v)) {
                     Ok(_) => (),
                     Err(_) => return Err(Error),
                 }
@@ -75,6 +87,42 @@ impl<'a> MSP<'a> {
             };
         }
         Ok(res_l)
+    }
+
+    pub fn prune<'b, 'c>(t: AccessTree<'a, 'b>, atts: &[&'c str]) -> Result<Vec<&'a str, S>, Error> {
+        Self::prune_rec(t, &t[0], atts)
+    }
+
+    fn prune_rec<'c>(t: &[AccessNode<'a>], current_node: &AccessNode<'a>, atts: &[&str]) -> Result<Vec<&'a str, S>, Error> {
+         let req = match current_node {
+            AccessNode::And(l, r) => {
+                let mut l_req = Self::prune_rec(t, &t[*l as usize], atts)?;
+                let r_req = Self::prune_rec(t, &t[*r as usize], atts)?;
+                l_req.extend(r_req.into_iter());
+                l_req
+            },
+            AccessNode::Or(l, r) => { // return the left side if it is satisfied, otherwise the right
+                if let Ok(req) = Self::prune_rec(t, &t[*l as usize], atts) {
+                    req
+                } else {
+                    match Self::prune_rec(t, &t[*r as usize], atts) {
+                        Ok(req) => req,
+                        Err(_) => return Err(Error),
+                    }
+                }
+            },
+            AccessNode::Leaf(a) => {
+                match atts.iter().filter(|a_| *a_ == a).count() > 0 {
+                    false => return Err(Error),
+                    true => {
+                        let mut r = Vec::new();
+                        r.push(*a)?;
+                        r
+                    },
+                }
+            }
+        };
+        Ok(req)
     }
 }
 
@@ -90,14 +138,16 @@ impl From<()> for Error {
     }
 }
 
+impl From<&str> for Error {
+    fn from(_: &str) -> Error{
+        Error
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    // extern crate std;
-    // use std::prelude::v1::*;
     use super::*;
     use super::Label::*;
-
-    use std::println;
 
     #[test]
     fn tree_to_msp() {
@@ -123,20 +173,73 @@ mod tests {
         ];
 
         let mut msp = MSP::from_access_tree(tree).unwrap().0;
-        println!("{:?}", &msp);
-
 
         // sort by attribute name -> otherwise the comparison would fail 
         // this is ugly because we need to extract the str-slice that is contained either in the Attribute or NotAttribute variant of the Label enum
         msp.sort_by(|(l1, _), (l2,_)| {
-            match (l1, l2) {
-                (Attribute(s1), Attribute(s2)) | (NotAttribute(s1), Attribute(s2)) | (Attribute(s1), NotAttribute(s2)) | (NotAttribute(s1), NotAttribute(s2)) => s1.cmp(s2),
-            }
+            l1.get_str().cmp(l2.get_str())
         });
 
         // println!("{:?}", &msp);
         
         assert_eq!(&msp[..], expected);
         
+    }
+
+    #[test]
+    fn test_prune() {
+        // access policy: A AND (D OR (B AND C))
+        let tree = &[
+            AccessNode::And(1, 2),  // 0
+            AccessNode::Leaf("A"),  // 1
+            AccessNode::Or(3, 4),   // 2
+            AccessNode::Leaf("D"),  // 3
+            AccessNode::And(5, 6),  // 4
+            AccessNode::Leaf("B"),  // 5
+            AccessNode::Leaf("C"),  // 6
+        ];
+
+        let att_all = &["A", "B", "C", "D"];
+        assert_eq!(MSP::prune(tree, att_all).unwrap(), &["A", "D"]);
+
+        let att_satisfies = &["B", "A", "C"];
+        assert_eq!(MSP::prune(tree, att_satisfies).unwrap(), &["A", "B", "C"]);
+
+        let att_insufficient = &["B", "C"];
+        assert_eq!(MSP::prune(tree, att_insufficient), Err(Error));
+    }
+
+    #[test]
+    fn test_reconstruct() {
+        let tree = &[
+            AccessNode::And(1, 2),  // 0
+            AccessNode::Leaf("A"),  // 1
+            AccessNode::Or(3, 4),   // 2
+            AccessNode::Leaf("D"),  // 3
+            AccessNode::And(5, 6),  // 4
+            AccessNode::Leaf("B"),  // 5
+            AccessNode::Leaf("C"),  // 6
+        ];
+
+
+        let msp = MSP::from_access_tree(tree).unwrap();
+
+        let m_i: Vec<(Label, Vec<i8, S>), S> = msp.0.into_iter().filter(|(l, _)| l.get_str() == "A" || l.get_str() == "D").collect();
+
+        let pruned = MSP::prune(tree, &["A", "D"]).unwrap();
+
+        let mut res: Vec<i8, S> = Vec::new();
+        res.resize(m_i[0].1.len(), 0).unwrap();
+        for (l, row) in m_i.into_iter() {
+            let coeff = if pruned.iter().filter(|x| l.get_str() == **x).count() != 0 { 1 } else { 0 };
+            for (acc, x) in res.iter_mut().zip(row.iter()) {
+                *acc += coeff * x;
+            }
+        }
+
+        let mut solution_vec: Vec<i8, S> = Vec::new();
+        solution_vec.push(1).unwrap();
+        solution_vec.resize(res.len(), 0).unwrap();
+        assert_eq!(res, solution_vec);
     }
 }
